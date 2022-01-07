8ae85f3b456f1f0438a0c989240a81db66040da7
Steven Garberg <steven.garberg.jr@gmail.com>
2022-01-06 23:02:23 -0500

    Added post stream project

---
+++ ConnectFour.App/Models/GameBoard.cs
@@ -0,0 +1,14 @@
+﻿namespace ConnectFour.App.Models;
+
+public class GameBoard
+{
+    public bool?[][] State { get; private set; } = new bool?[6][];
+
+    public GameBoard()
+    {
+        for (var index = 0; index < State.Length; index++)
+        {
+            State[index] = new bool?[7];
+        }
+    }
+}
